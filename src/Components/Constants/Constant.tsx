37c7f610d16708847fbb571d89bd4b1c547b1b6f
Parul Taneja <parul.taneja@trantorchd.com>
2022-08-16 15:27:04 +0530

    added redux

--- src/Components/Constants/Constant.tsx
+++ src/Components/Constants/Constant.tsx
@@ -1,9 +1,10 @@
-export const POPUP_MESSAGES={
-    CONFIRMATION_MESSAGE: "Are you sure you want to delete this blog? if pressed on confirm it can't be seen again",
-    DISCARD_CHANGES_MESSAGE:"Are you sure you want to discard new added changes",
-}
+export const POPUP_MESSAGES = {
+  CONFIRMATION_MESSAGE:
+    "Are you sure you want to delete this blog? if pressed on confirm it can't be seen again",
+  DISCARD_CHANGES_MESSAGE: "Are you sure you want to discard new added changes",
+};
 
-export const PLACEHOLDER_MESSAGES={
-    ENTER_TEXT: "Enter text here",
-    ENTER_TITLE: "Enter title here",
-}
+export const PLACEHOLDER_MESSAGES = {
+  ENTER_TEXT: "Enter text here",
+  ENTER_TITLE: "Enter title here",
+};
--- src/index.tsx
+++ src/index.tsx
@@ -4,13 +4,22 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+import { ConfigureStore } from "./Redux/store/index";
+
+const { persistor, store } = ConfigureStore();
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <PersistGate persistor={persistor} loading={null}>
+      <Provider store={store}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </Provider>
+    </PersistGate>
   </React.StrictMode>,
 );
 
